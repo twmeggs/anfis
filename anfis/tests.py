514a1aec0af02aa55f8e6979f57c3d969128d852
Tim Meggs <twmeggs@gmail.com>
2018-06-01 17:09:37 +0100

    Fix for Issue 3: altered membership function params to improve results

--- anfis/tests.py
+++ anfis/tests.py
@@ -6,13 +6,13 @@ ts = numpy.loadtxt("trainingSet.txt", usecols=[1,2,3])#numpy.loadtxt('c:\\Python
 X = ts[:,0:2]
 Y = ts[:,2]
 
-mf = [[['gaussmf',{'mean':-11.,'sigma':5.}],['gaussmf',{'mean':-8.,'sigma':5.}],['gaussmf',{'mean':-14.,'sigma':20.}],['gaussmf',{'mean':-7.,'sigma':7.}]],
-            [['gaussmf',{'mean':-10.,'sigma':20.}],['gaussmf',{'mean':-20.,'sigma':11.}],['gaussmf',{'mean':-9.,'sigma':30.}],['gaussmf',{'mean':-10.5,'sigma':5.}]]]
+mf = [[['gaussmf',{'mean':0.,'sigma':1.}],['gaussmf',{'mean':-1.,'sigma':2.}],['gaussmf',{'mean':-4.,'sigma':10.}],['gaussmf',{'mean':-7.,'sigma':7.}]],
+            [['gaussmf',{'mean':1.,'sigma':2.}],['gaussmf',{'mean':2.,'sigma':3.}],['gaussmf',{'mean':-2.,'sigma':10.}],['gaussmf',{'mean':-10.5,'sigma':5.}]]]
 
 
 mfc = membership.membershipfunction.MemFuncs(mf)
 anf = anfis.ANFIS(X, Y, mfc)
-anf.trainHybridJangOffLine(epochs=10)
+anf.trainHybridJangOffLine(epochs=20)
 print round(anf.consequents[-1][0],6)
 print round(anf.consequents[-2][0],6)
 print round(anf.fittedValues[9][0],6)
